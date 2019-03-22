45241dd02a7c7f3cb553ed8c9cb84d1add9074bd
Yahia Hasan <yehia4747@gmail.com>
2019-03-22 23:01:32 +0200

    finish ch04_ex_05

---
+++ chapter04/chapter04_ex05.cpp
@@ -0,0 +1,34 @@
+#include "../headers/std_lib_facilities.h"
+
+vector<int> getMedian(int start, int end);
+
+int main()
+{
+    double operand1 = -1, operand2 = -1;
+    char operation = 0;
+    cout << "Enter two numbers followed by an operation: ";
+    cin >> operand1 >> operand2 >> operation;
+    switch (operation)
+    {
+        case '+':
+            cout << "The sum of " << operand1 << " and " << operand2 << " is " << operand1 + operand2 << endl;
+            break;
+
+        case '-':
+            cout << "The difference between " << operand1 << " and " << operand2 << " is " << operand1 - operand2 << endl;
+            break;
+
+        case '*':
+            cout << "The product of " << operand1 << " and " << operand2 << " is " << operand1 * operand2 << endl;
+            break;
+
+        case '/':
+            cout << "The division of " << operand1 << " by " << operand2 << " is " << operand1 / operand2 << endl;
+            break;
+    
+        default:
+            cout << "Invalid operation." << endl;
+            break;
+    }
+    return 0;
+}
