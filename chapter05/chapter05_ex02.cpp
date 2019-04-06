80fc8e280a6019f8a3437b2a1156905daa988b56
Yahia Hasan <yehia4747@gmail.com>
2019-04-06 20:11:15 +0200

    finish ch05e02

---
+++ chapter05/chapter05_ex02.cpp
@@ -0,0 +1,17 @@
+#include "../headers/std_lib_facilities.h"
+
+
+double ctok(double c) // converts Celsius to Kelvin
+{
+    int k = c + 273.15;
+    return k;
+}
+
+int main()
+{
+    double c = 0; // declare input variable
+    cin >> c; // retrieve temperature to input variable
+    double k = ctok(c); // convert temperature
+    cout << k << "\n" ; 
+    return 0;
+}
